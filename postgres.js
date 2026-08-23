4bd99c86dcb6eb98bf2471b8190ad28d51b16c02
Tina <contactina101@gmail.com>
2026-08-23 12:38:34 +0545

    Preserved PostgreSQL date values

--- postgres.js
+++ postgres.js
@@ -1,4 +1,8 @@
-const { Pool } = require("pg");
+//1082 is PostgreSQL's type ID for date.
+//Don't turn PostgreSQL dates into JavaScript Date objects. Just give me the original YYYY-MM-DD string.
+const { Pool, types } = require("pg");
+
+types.setTypeParser(1082, value => value);
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
